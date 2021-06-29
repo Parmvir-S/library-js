75bacf1dded10a19d51c5456dae22dd39b7e557e
Parmvir-S <parmvir.shergill@gmail.com>
2021-06-28 23:24:33 -0700

    Added: increase bookList items, add book obj to list, determine how checkbox is checked, finished up some design, made mobile repsonsive

--- script.js
+++ script.js
@@ -1,33 +1,34 @@
-// let myLibrary = [];
 
-// function Book(title, author, pages, readStatus) {
-//     this.title = title;
-//     this.author = author;
-//     this.pages = pages;
-//     this.readStatus = readStatus;
-//     this.fullString = function() {
-//         return this.title + " by " + this.author + ", has " + this.pages + " pages";
-//     }
-// }
 
-// let buttonGo = document.querySelector("button");
-// buttonGo.addEventListener("click", addBookToLibrary)
+let myLibrary = [];
 
-// function addBookToLibrary() {
-//     let title = prompt("Title: ");
-//     let author = prompt("Author: ");
-//     let pages = Number(prompt("Pages: "));
-//     let status = Boolean(prompt("Read: true/false?"));
+function Book(title, author, pages, readStatus) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.readStatus = readStatus;
+    this.fullString = function() {
+        return this.title + " by " + this.author + ", has " + this.pages + " pages";
+    }
+}
 
-//     let newBook = new Book(title, author, pages, status);
-//     myLibrary.push(newBook)
-//     displayBook(newBook)
-// }
+function addBookToLibrary() {
+    let author = document.getElementById("author").value;
+    let title = document.getElementById("title").value;
+    let pages = document.getElementById("pages").value;
+    let status = document.getElementById("status").checked;
+    let newBook = new Book(title, author, pages, status);
+    myLibrary.push(newBook)
+    bookCount.textContent = myLibrary.length;
+}
 
-// const displayContainer = document.querySelector(".container");
-// function displayBook(newBook) {
-//     let newDiv = document.createElement("div");
-//     newDiv.style.cssText = "border: 2px solid black;";
-//     newDiv.textContent = newBook.fullString();
-//     displayContainer.appendChild(newDiv);
-// }
+const addBookbutton = document.querySelector("#addBookButton");
+addBookbutton.addEventListener("click", addBookToLibrary);
+
+
+const clearAllBooksButton = document.querySelector(".clearbutton");
+clearAllBooksButton.addEventListener("click", () => {
+    myLibrary = [];
+})
+
+const bookCount = document.querySelector("#bookCount");
